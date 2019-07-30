7bc3bf1b8e7fb6c1258bcc12d7287a78dc93f824
ParkYeRim <pako1234@naver.com>
2019-07-30 13:50:56 +0900

    initiall commit

--- index.js
+++ index.js
@@ -1,23 +1,41 @@
-const express = require("express");
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import { runInNewContext } from "vm";
+
 const app = express();
 
 const PORT = 4000;
 
-function handleListening() {
+const handleListening = () => console.log(`Listening on : http://localhost:${PORT}`);
+/*function handleListening() {
   console.log(`Listening on: http://localhost:${PORT}`);
-}
+}*/
 
-function handleHome(req, res) {
-  console.log(req);
-  res.send("Hi from Home");
-}
+const handleHome = (req, res) => res.send("Hello from home");
 
-function handleProfile(req, res) {
-  res.send("You are on my profile");
-}
+const handleProfile = (req, res) => res.send("You are on my profile");
+
+const middleware = (req, res, next) => {
+  res.send("not happening");
+};
+
+const betweenHome = (req, res, next) => {
+  console.log("I'm between");
+  next();
+};
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(betweenHome);
+app.use(helmet());
+app.use(morgan("dev"));
+
+app.get("/", middleware, handleHome);
 
 app.get("/profile", handleProfile);
-
-app.get("/", handleHome);
 
 app.listen(PORT, handleListening);
